b62c1e0ac5dfba9d5f2f49bc0b77702352ed71b9
Ahmed <sultan_902002@yahoo.com>
2019-12-15 12:57:06 -0700

    add navbar and created navbar story for styling and testing

---
+++ src/stories/2-navBar.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ButtonAppBar from "components/Navbar";
+
+
+export default {
+  title: "Navigation Bar"
+};
+
+export const NavBar = () => (
+  <ButtonAppBar >
+  </ButtonAppBar>
+);

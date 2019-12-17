1a47e493d8bce578b74d0291edcb09ba7ebf7fda
Ahmed <sultan_902002@yahoo.com>
2019-12-16 20:28:45 -0700

    start builing client categories component and add stories

---
+++ src/components/clientCategories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default function Clientcategories() {
+
+  return (
+    <div></div>
+  );
+}
---
+++ src/stories/3-clientCategories,stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Clientcategories from "components/clientCategories";
+
+
+export default {
+  title: "Client categories"
+};
+
+export const Categories = () => (
+  <Clientcategories></Clientcategories>
+);

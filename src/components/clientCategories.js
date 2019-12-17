aa3344a9bd9d20e8398334bc2e20eadc7e282119
Ahmed <sultan_902002@yahoo.com>
2019-12-16 21:43:44 -0700

    build category component

--- src/components/clientCategories.js
+++ src/components/clientCategories.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import "components/styles.scss";
 
 
-export default function Clientcategories() {
+export default function Clientcategories(props) {
+
+  const categoryList = props.categories.map(category => {
+    return (
+      <div className="categoryItem"
+        key={category.id}
+        id={category.id}
+        categoryName={category.categoryname}
+        accountantName={category.accountantname}
+      >{category.categoryname}</div>
+    )
+  })
 
   return (
-    <div></div>
+    <div className="categoryList">
+      {categoryList}
+      <img
+          style={{ cursor: 'pointer' }}
+          src="images/add-64.png"
+          alt="Create"
+          onClick={() => console.log("create category")}
+        />
+    </div>
   );
 }

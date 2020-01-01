53b9e3a606fbf67333c36118d10bc6afbb8c722a
Ahmed <sultan_902002@yahoo.com>
2020-01-01 14:47:04 -0700

    switch accountant and user login positions and pass user name to display when loggedin

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,56 +1,56 @@
 import React from 'react';
 import Collapsible from 'react-collapsible';
 import "components/Navbar/styles.scss";
+import  { useApplicationData }  from "hooks/useApplicationData";
 
 
 
 export default function NavBar(props) {
-  const name = "sup"
+  const {
+    logout
+  } = useApplicationData();
   let mode = props.name ? "LOGGEDIN" : "LOGGEDOUT";
 
   return (
     <section className="topbar">
       <div className="brand" onClick={() => console.log("hello")}>SmartDocs</div>
-      
+
       {mode === "LOGGEDOUT" && (
-      <div className="notLoggedInMenu">
-        <div className='hamburgerMenu'>
-          <Collapsible trigger="Start Here">
-            <div className="accountant">Accountant
-              <div className="auth" onClick={() => console.log("hello")}>Register</div>
-              <div className="auth" onClick={() => console.log("hello")}>Login</div>
-              </div>
+        <div className="notLoggedInMenu">
+          <div className='hamburgerMenu'>
+            <Collapsible trigger="Start Here">
               <div className="accountant">Client
               <div className="auth" onClick={() => console.log("hello")}>Register</div>
-              <div className="auth" onClick={() => console.log("hello")}>Login</div>
+                <div className="auth" onClick={() => console.log("hello")}>Login</div>
+              </div>
+              <div className="accountant">Accountant
+              <div className="auth" onClick={() => console.log("hello")}>Register</div>
+                <div className="auth" onClick={() => console.log("hello")}>Login</div>
               </div>
             </Collapsible>
-            {/* <div className="hamburgerbar"></div>
-            <div className="hamburgerbar"></div>
-            <div className="hamburgerbar"></div> */}
-        </div>
-        <div className='dropMenus'>
-          <div className="menu">
-            <Collapsible trigger="Accountant">
-              <div className="auth" onClick={() => console.log("hello")}>Register</div>
-              <div className="auth" onClick={() => console.log("hello")}>Login</div>
-            </Collapsible>
           </div>
+          <div className='dropMenus'>
+            <div className="menu">
+              <Collapsible trigger="Client">
+                <div className="auth" onClick={() => console.log("hello")}>Register</div>
+                <div className="auth" onClick={() => console.log("hello")}>Login</div>
+              </Collapsible>
+            </div>
+            <div className="menu--coma">/</div>
+            <div className="menu">
+              <Collapsible trigger="Accountant">
+                <div className="auth" onClick={() => console.log("hello")}>Register</div>
+                <div className="auth" onClick={() => console.log("hello")}>Login</div>
+              </Collapsible>
+            </div>
 
-          <div className="menu--coma">/</div>
-          <div className="menu">
-            <Collapsible trigger="Client">
-              <div className="auth" onClick={() => console.log("hello")}>Register</div>
-              <div className="auth" onClick={() => console.log("hello")}>Login</div>
-            </Collapsible>
           </div>
-        </div>
-      </div>)}
+        </div>)}
 
       {mode === "LOGGEDIN" && (
-      <div className="loggedInMenu">
-         {name} -> <span onClick={() => console.log("hello")}>Logout</span>
-      </div>)}
+        <div className="loggedInMenu">
+          {props.name} --> <span onClick={() => logout()}>Logout</span>
+        </div>)}
     </section>
   );
 }

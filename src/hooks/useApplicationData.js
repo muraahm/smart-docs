8299902d2c58fb406bb96c0e37d9a8cd16a8ee67
Ahmed <sultan_902002@yahoo.com>
2020-01-02 08:34:10 -0700

    add accountant api call to the login function and add three calls to get the app ready if user loggedin

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import config from '../config'
 import { useEffect, useReducer } from "react";
-import { reducer, SET_USER_INFO, SET_USER_CATEGORIES } from "../reducer/application";
-import { Categories } from "stories/3-clientCategories.stories";
+import { reducer, SET_USER_INFO, SET_USER_CATEGORIES, SET_ACCOUNTANTS, SET_APP_DATA } from "../reducer/application";
 
 export function useApplicationData() {
   const [state, dispatch] = useReducer(reducer,
     {
       // INIT STATE
       userInfo: {},
-      userCategories: []
+      userCategories: [],
+      accountants: []
     }
   );
 
@@ -20,40 +20,89 @@ export function useApplicationData() {
         dispatch({ type: SET_USER_INFO, value: userInfo });
       })
       .then(
-        axios.get(`${config.API_PATH}/api/user/categories/list/${email}`, { email, password })
+        axios.get(`${config.API_PATH}/api/user/categories/list/${email}`, { email })
           .then(response => {
             const userCategories = response.data;
             dispatch({ type: SET_USER_CATEGORIES, value: userCategories });
           }))
+      .then(
+        axios.get(`${config.API_PATH}/api/accountants`)
+          .then(response => {
+            const accountants = response.data;
+            dispatch({ type: SET_ACCOUNTANTS, value: accountants });
+          })
+      )
   };
 
   const listUserCategories = (email) => {
     axios.get(`${config.API_PATH}/api/user/categories/list/${email}`, { email })
-          .then(response => {
-            const userCategories = response.data;
-            dispatch({ type: SET_USER_CATEGORIES, value: userCategories });
-          })
+      .then(response => {
+        const userCategories = response.data;
+        dispatch({ type: SET_USER_CATEGORIES, value: userCategories });
+      })
   }
 
 
   const logout = () => {
-    // const userInfo = {};
-    // const userCategories = [];
-    // dispatch({ type: SET_USER_INFO, SET_USER_CATEGORIES, value: userInfo, userCategories });
+    const userInfo = {};
+    const userCategories = [];
+    dispatch({ type: SET_USER_INFO, SET_USER_CATEGORIES, value: userInfo, userCategories });
   }
 
   const createCategory = (name, email, acct_company) => {
     return axios.put(`${config.API_PATH}/api/users/create/category`, { name, email, acct_company })
       .then(response => {
-        const userCategories = response.data;
-        dispatch({ type: SET_USER_CATEGORIES, value: userCategories });
+        axios.get(`${config.API_PATH}/api/user/categories/list/${email}`, { email })
+          .then(response => {
+            const userCategories = response.data;
+            dispatch({ type: SET_USER_CATEGORIES, value: userCategories });
+          })
       })
 
   }
 
+  const register = (name, email, password) => {
+    return axios.put(`${config.API_PATH}/api/users/register`, { name, email, password })
+      .then(response => {
+        const userInfo = response.data;
+        dispatch({ type: SET_USER_INFO, value: userInfo });
+
+      })
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("together::token");
+    Promise.all([
+      axios.get(`${config.API_PATH}/api/accountants`),
+      axios.post(`${config.API_PATH}/api/user`, { token }),
+      axios.get(`${config.API_PATH}/api/user/categories/`, { token })
+    ])
+      .then((all) => {
+        console.log(all[0].data)
+        dispatch({
+          type: SET_APP_DATA,
+          value: {
+            accountants: all[0].data,
+            userInfo: all[1].data,
+            userCategories: all[2].data
+          }
+          
+        });
+      })
+      .catch(err => {
+        console.log(err.response.status);
+        // console.log(err.response.headers);
+        // console.log(err.response.data);
+      });
+
+  }, []);
+
+
+
   return {
     state,
     login,
+    register,
     logout,
     createCategory,
     listUserCategories

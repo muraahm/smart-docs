4fec32f1a73a405dc4c6d46257a0ae03a68414a6
Ahmed <sultan_902002@yahoo.com>
2020-01-05 20:43:32 -0700

    edit .env.example and add heroku url for api calls

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   API_PATH: {
     development: 'http://localhost:3002',
-    // production: 'https://together-lhl-api.herokuapp.com'
+    production: 'https://smart-docs1.herokuapp.com'
   }[process.env.NODE_ENV || 'development']
 }
